0bf227a4aa8f6ea5f8a562d6ed822e393c2d7445
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2020-03-21 16:13:20 +0000

    Add project files.

---
+++ DigitsRecognizerML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DigitsRecognizerML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
